2c0b3d07f38eb241374b433fc3d51a1fa1c06d60
MughniRayhan <tishtisha13@gmail.com>
2025-09-01 20:52:31 +0600

    add verifyFbToken for verification

---
+++ uni-clubs-server-side/middleware/verifyFirebaseToken.js
@@ -0,0 +1,22 @@
+const admin = require("../config/firebase");
+
+const verifyFbToken = async(req,res,next)=>{
+    const authHeaders = req.headers.authorization;
+    if(!authHeaders){
+      return res.status(401).send({message: "unauthorized access"})
+    }
+
+    const token = authHeaders.split(' ')[1];
+    if(!token){
+      return res.status(401).send({message: "unauthorized access"})
+    }
+      try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.decoded = decodedToken;
+    next();
+  } catch (error) {
+    res.status(403).send({ message: "Forbidden access" });
+  }
+  }
+
+module.exports = verifyFbToken;
--- uni-clubs-server-side/routes/userRoutes.js
+++ uni-clubs-server-side/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {createUser, getUsers, getUserById, updatedUser, deleteUser} = require('../controllers/userController');
+const {createUser} = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/user',createUser);
