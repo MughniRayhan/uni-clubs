f744bade735ca568a2bd9c7da7f4a7d93e49ed8e
MughniRayhan <tishtisha13@gmail.com>
2025-09-01 16:51:07 +0600

    add login

--- uni-clubs-client-side/src/Pages/Auth/SignIn/Login.jsx
+++ uni-clubs-client-side/src/Pages/Auth/SignIn/Login.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React from 'react'
+import { useForm } from "react-hook-form"
+import { Link, useLocation, useNavigate } from 'react-router';
+import SocialLogin from '../SocialLogin/SocialLogin';
+import { toast } from 'react-toastify';
+import UseAuth from '../../../Hooks/UseAuth';
 
-const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    );
-};
+function Login() {
+    const {register, handleSubmit, formState: {errors}} = useForm();
+    const {signIn} = UseAuth();
+    const location = useLocation();
+    const navigate = useNavigate();
+  
 
-export default Login;
+    const onSubmit = (data) => {
+       
+        signIn(data.email, data.password)
+            .then((result) => {
+                toast.success("Successfully logged in")
+                navigate(location.state || '/');
+            })
+            .catch((error) => {
+                console.error(error.message);
+            })
+    }
+  return (
+    <div className=' my-10 sm:w-[70%] mx-auto w-full shadow-lg md:shadow-none  '>
+        <form onSubmit={handleSubmit(onSubmit)} className='card-body'>
+           <div className='mb-2'>
+             <h2 className='font-extrabold sm:text-3xl text-xl mb-1 '>Welcome Back</h2>
+            
+           </div>
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input type="email"  
+          {...register("email",{ required: true})} 
+          className="input" placeholder="Email" />
+          {
+            errors.email?.type === 'required' && <span className='text-red-500'>Password is required</span>
+          }
+
+          <label className="label">Password</label>
+          <input type="password"  
+          {...register("password",{ required: true})} 
+          className="input" placeholder="Password" />
+          {
+            errors.password?.type === 'required' && <span className='text-red-500'>Password is required</span>
+          }
+          
+          <button className="btn btn-neutral mt-4 sm:w-[330px] bg-primary   font-bold border-none">Login</button>
+           <div className='mt-2 text-base'>Don't have any account? <Link to='/auth/register' className='text-secondary font-semibold underline dark:text-green-600'>Register</Link></div>
+          
+          <SocialLogin/>
+        </fieldset>
+    </form>
+    </div>
+  )
+}
+
+export default Login
--- uni-clubs-client-side/src/Pages/Auth/SignUp/SignUp.jsx
+++ uni-clubs-client-side/src/Pages/Auth/SignUp/SignUp.jsx
@@ -41,7 +41,7 @@ const onSubmit = (data) => {
         <div className=' my-10 sm:w-[70%] mx-auto w-full shadow-lg md:shadow-none'>
         <form onSubmit={handleSubmit(onSubmit)} className='card-body'>
            <div className='mb-2'>
-             <h2 className='font-extrabold sm:text-3xl text-xl mb-1 text-accent dark:text-orange-500'>Create an Account</h2>
+             <h2 className='font-extrabold sm:text-3xl text-xl mb-1  '>Create an Account</h2>
             
            </div>
 
@@ -77,7 +77,7 @@ const onSubmit = (data) => {
             errors.password?.type === 'minLength' && <span className='text-red-500'>Must have to 6 length</span>
           }
          
-          <button className="btn btn-neutral mt-4 sm:w-[330px] bg-primary  font-bold border-none dark:bg-orange-500">Register</button>
+          <button className="btn btn-neutral mt-4 sm:w-[330px] bg-primary  font-bold border-none">Register</button>
           <div className='mt-2 text-base'>Allready have an account?    <Link to='/auth/login' className='text-secondary font-semibold underline dark:text-green-600'>   Login</Link></div>
          <SocialLogin />
         </fieldset>
